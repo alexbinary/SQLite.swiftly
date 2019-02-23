52e5d292a8b8abf2fab0d3c082de5ca73cb371f4
Alexandre Bintz <alexandre@bintz.io>
2019-02-23 21:10:37 +0100

    setup demo project

---
+++ SQLite.swiftly Demo App/SQLite.swiftly Demo App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SQLite.swiftly Demo App
+//
+//  Created by Alexandre Bintz on 23/02/2019.
+//  Copyright © 2019 Alexandre Bintz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
