3ee2129f27d9c6ce8cffd21068f2666488c1adf4
Alexandre Bintz <alexandre@bintz.io>
2019-02-24 11:29:17 +0100

    start writing actual code for demo app

--- Demo App/SQLite.swiftly Demo App/ViewController.swift
+++ Demo App/SQLite.swiftly Demo App/ViewController.swift
@@ -12,9 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        run()
     }
 
-
+    func run() {
+        
+        let applicationSupportUrl = try! FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        
+        let folderUrl = applicationSupportUrl.appendingPathComponent(Bundle.main.bundleIdentifier!)
+        
+        let databaseUrl = folderUrl.appendingPathComponent("db.sqlite")
+        
+        print(databaseUrl.path)
+        
+        writeToDatabase(url: databaseUrl)
+        readFromDatabase(url: databaseUrl)
+    }
+    
+    func writeToDatabase(url: URL) {
+        
+        let connection = SQLite_Connection(toDatabaseAt: url)
+    }
+    
+    func readFromDatabase(url: URL) {
+        
+    }
 }
 
