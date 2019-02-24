35ab90a9191213b48a682c17e9a1cc9d826c6d5b
Alexandre Bintz <alexandre@bintz.io>
2019-02-24 11:47:55 +0100

    complete demo app code

--- Demo App/SQLite.swiftly Demo App/ViewController.swift
+++ Demo App/SQLite.swiftly Demo App/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let column1 = SQLite_ColumnDescription(name: "c1", type: .char(size: 255), nullable: false)
+    let column2 = SQLite_ColumnDescription(name: "c2", type: .char(size: 255), nullable: false)
+    
+    lazy var table = SQLite_TableDescription(name: "demoTable", columns: [column1, column2])
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,7 +29,11 @@ class ViewController: UIViewController {
         
         let folderUrl = applicationSupportUrl.appendingPathComponent(Bundle.main.bundleIdentifier!)
         
+        try! FileManager.default.createDirectory(at: folderUrl, withIntermediateDirectories: true, attributes: nil)
+        
         let databaseUrl = folderUrl.appendingPathComponent("db.sqlite")
+        
+        try? FileManager.default.removeItem(at: databaseUrl)
         
         print(databaseUrl.path)
         
@@ -33,10 +44,30 @@ class ViewController: UIViewController {
     func writeToDatabase(url: URL) {
         
         let connection = SQLite_Connection(toDatabaseAt: url)
+        
+        connection.createTable(describedBy: table)
+        
+        let statement = SQLite_InsertStatement(insertingIntoTable: table, connection: connection)
+        
+        statement.insert([
+            column1: "hello",
+            column2: "world",
+        ])
     }
     
     func readFromDatabase(url: URL) {
         
+        let connection = SQLite_Connection(toDatabaseAt: url)
+        
+        let statement = SQLite_SelectStatement(selectingFromTable: table, connection: connection)
+        
+        for row in statement.readAllRows() {
+            
+            for (column, value) in row {
+            
+                print("\(column.name) : \(String(describing: value))")
+            }
+        }
     }
 }
 
