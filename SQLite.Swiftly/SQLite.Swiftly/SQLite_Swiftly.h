f92b3635a50130e83b4a9cca4618a795c2be0d97
Alexandre Bintz <alexandre@bintz.io>
2019-03-10 16:32:26 +0100

    ⚗️ create framework xcode project

---
+++ SQLite.Swiftly/SQLite.Swiftly/SQLite_Swiftly.h
@@ -0,0 +1,19 @@
+//
+//  SQLite_Swiftly.h
+//  SQLite.Swiftly
+//
+//  Created by Alexandre Bintz on 10/03/2019.
+//  Copyright © 2019 Alexandre Bintz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SQLite_Swiftly.
+FOUNDATION_EXPORT double SQLite_SwiftlyVersionNumber;
+
+//! Project version string for SQLite_Swiftly.
+FOUNDATION_EXPORT const unsigned char SQLite_SwiftlyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SQLite_Swiftly/PublicHeader.h>
+
+
