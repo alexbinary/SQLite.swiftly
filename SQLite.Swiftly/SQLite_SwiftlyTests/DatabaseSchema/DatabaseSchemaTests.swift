e1e12001b3572b094d986302c89e1113efef78ec
Alexandre Bintz <alexandre@bintz.io>
2019-03-14 22:50:34 +0100

    ✅ add unit tests for column

--- SQLite.Swiftly/SQLite_SwiftlyTests/DatabaseSchema/DatabaseSchemaTests.swift
+++ SQLite.Swiftly/SQLite_SwiftlyTests/DatabaseSchema/DatabaseSchemaTests.swift
@@ -7,16 +7,29 @@ import XCTest
 class DatabaseSchema: XCTestCase {
 
  
-    func test_columnType_shouldProduceCorrectSQL() {
+    func test_columnType_shouldHaveCorrectSqlRepresentation() {
         
         [
-            (type: SQLite_ColumnType.char(size: 11), expectedSqlRepresentation: "CHAR(11)"),
-            (type: SQLite_ColumnType.char(size: 64), expectedSqlRepresentation: "CHAR(64)"),
-            (type: SQLite_ColumnType.bool, expectedSqlRepresentation: "BOOL"),
+            (type: SQLite_ColumnType.char(size: 11), expectedSql: "CHAR(11)"),
+            (type: SQLite_ColumnType.char(size: 64), expectedSql: "CHAR(64)"),
+            (type: SQLite_ColumnType.bool, expectedSql: "BOOL"),
             
-        ].forEach { (type, expectedSQL) in
+        ].forEach { (type, expectedSql) in
          
-            XCTAssertEqual(type.sqlRepresentation, expectedSQL, "SQL representation differ from expected.")
+            XCTAssertEqual(type.sqlRepresentation, expectedSql, "SQL representation differ from expected.")
+        }
+    }
+    
+    
+    func test_column_shouldHaveCorrectSQLRepresentation() {
+        
+        [
+            (column: SQLite_ColumnDescription(name: "col1", type: .bool, nullable: true), expectedSql: "col1 \(SQLite_ColumnType.bool.sqlRepresentation) NULL"),
+            (column: SQLite_ColumnDescription(name: "col1", type: .bool, nullable: false), expectedSql: "col1 \(SQLite_ColumnType.bool.sqlRepresentation) NOT NULL"),
+        
+        ].forEach { (column, expectedSql) in
+            
+            XCTAssertEqual(column.sqlRepresentation, expectedSql, "SQL representation differ from expected.")
         }
     }
 }
