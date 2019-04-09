df9b931a999d19ab4cce335c9664a9082a60c4bc
Alexandre Bintz <alexandre@bintz.io>
2019-04-09 20:05:14 +0200

    ♻️ rewrite column tests

--- SQLite.Swiftly/SQLite_SwiftlyTests/DatabaseSchema/ColumnDescriptionTests.swift
+++ SQLite.Swiftly/SQLite_SwiftlyTests/DatabaseSchema/ColumnDescriptionTests.swift
@@ -9,7 +9,13 @@ class ColumnDescriptionTests: XCTestCase {
  
     func test_shouldHaveCorrectSQLRepresentation() {
         
-        XCTAssertEqual(ColumnDescription(name: "col1", type: .bool, nullable: true).sqlRepresentation, "col1 \(ColumnType.bool.sqlRepresentation) NULL")
-        XCTAssertEqual(ColumnDescription(name: "col1", type: .bool, nullable: false).sqlRepresentation, "col1 \(ColumnType.bool.sqlRepresentation) NOT NULL")
+        XCTAssertEqual(
+            ColumnDescription(name: "col1", type: .bool, nullable: true).sqlRepresentation,
+            "col1 BOOL NULL"
+        )
+        XCTAssertEqual(
+            ColumnDescription(name: "col1", type: .char(size: 11), nullable: false).sqlRepresentation,
+            "col1 CHAR(11) NOT NULL"
+        )
     }
 }
